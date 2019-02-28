921eb979713ae3a8da5d0ec68c87554d2ad98fc6
Sproodigy <sproodigy@gmail.com>
2019-02-28 20:44:28 +0400

    Added a few attributes.

--- learn_web/scripts/domDragEventObj.js
+++ learn_web/scripts/domDragEventObj.js
@@ -9,3 +9,20 @@ function clipBoardEventOncutFunc() {
 function clipBoardEventOnpasteFunc() {
   document.getElementsByClassName("replace")[0].innerText = "Text has been inserted!";
 }
+
+function eventOndragstartFunc(event) {
+  event.dataTransfer.setData("text", event.target.id);
+  event.target.innerText = "Started dragging text!";
+}
+
+function eventOndragoverFunc(event) {
+  event.preventDefault();
+  document.getElementsByClassName("replace")[0].innerHTML = "The text is above the target point!";
+}
+
+function eventOndropFunc(event) {
+  event.preventDefault();
+  var data = event.dataTransfer.getData("text");
+  event.target.appendChild(document.getElementById(data));
+  document.getElementsByClassName("replace")[0].innerHTML = "Text dropped!";
+}
