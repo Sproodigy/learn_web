df59d42925368d45ef013f2f4855109726e2c53f
Sproodigy <sproodigy@gmail.com>
2019-03-02 09:46:49 +0400

    Added a few attributes.

--- learn_web/scripts/domDragEventObj.js
+++ learn_web/scripts/domDragEventObj.js
@@ -10,40 +10,42 @@ function clipBoardEventOnpasteFunc() {
   document.getElementsByClassName("replace")[0].innerText = "Text has been inserted!";
 }
 
-function eventOndragstartFunc(event) {
+
+
+function ondragstartFunc(event) {
   event.dataTransfer.setData("text", event.target.id);
   document.getElementsByClassName("replace")[0].innerHTML = "Started dragging text!";
-  document.getElementsByClassName("replace")[2].innerHTML = "";
 }
 
-function eventOndragoverFunc(event) {
+function ondragFunc(event, text) {
+  document.getElementsByClassName("replace")[1].innerHTML = text;
+}
+
+function ondragoverFunc(event, text) {
   event.preventDefault();
-  document.getElementsByClassName("replace")[1].innerHTML = "The text is above the target point!";
-  document.getElementsByClassName("replace")[2].innerHTML = "";
+  document.getElementsByClassName("replace")[2].innerHTML = text;
 }
 
-function eventOndragleaveFunc(event) {
+function ondragleaveFunc(event) {
   if ( event.target.className == "droptarget" ) {
-    document.getElementsByClassName("replace")[3].innerHTML = "Entered the dropzone";
-    event.target.style.border = "3px dotted green";
+    document.getElementsByClassName("replace")[3].innerHTML = "Left the dropzone!";
   }
 }
 
-function eventOndragenterFunc(event) {
+function ondragenterFunc(event) {
   if ( event.target.className == "droptarget" ) {
-    document.getElementsByClassName("replace")[3].innerHTML = "Left the dropzone";
-    event.target.style.border = "";
+    document.getElementsByClassName("replace")[3].innerHTML = "Entered the dropzone!";
   }
 }
 
-function eventOndropFunc(event) {
+function ondropFunc(event) {
   event.preventDefault();
   var data = event.dataTransfer.getData("text");
   event.target.appendChild(document.getElementById(data));
-  document.getElementsByClassName("replace")[0].innerHTML = "Text dropped!";
+  document.getElementsByClassName("replace")[1].innerHTML = "Text dropped!";
+  document.getElementsByClassName("replace")[2].innerHTML = "";
 }
 
-function eventOndragendFunc(event) {
-  document.getElementsByClassName("replace")[1].innerHTML = "";
-  document.getElementsByClassName("replace")[2].innerHTML = "Finished dragging an element.";
+function ondragendFunc(event) {
+  document.getElementsByClassName("replace")[0].innerHTML = "Finished dragging text!";
 }
