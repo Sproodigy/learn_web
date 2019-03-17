a392bb9c65bf64382d14ae2d24c73ead95885a53
Sproodigy <sproodigy@gmail.com>
2019-03-18 00:20:24 +0400

    The chapter "string" is complete.

--- learn_web/scripts/string.js
+++ learn_web/scripts/string.js
@@ -53,6 +53,22 @@ function JSStrFrameLeaveFunc(descNum, tableNum, noteNum) {
   y.style.background = "rgba(215, 62, 22, 0.92)";
 }
 
+function getConstructor() {
+  var string = document.getElementById('constructorText').innerHTML;
+  var target = document.getElementById('constructorReplace');
+  var result = string.constructor;
+
+  target.innerHTML = "Result: " + result;
+}
+
+function getLength() {
+  var string = document.getElementById('lengthText').innerHTML;
+  var target = document.getElementById('lengthReplace');
+  var result = string.length;
+
+  target.innerHTML = "Result: " + result;
+}
+
 function getCharAt() {
   var inputValue = document.getElementById('charAtValue').value;
   var string = document.getElementById('charAtText').innerText;
@@ -178,6 +194,13 @@ function compareStrings() {
   y.innerHTML = "Result: " + compare;
 }
 
+function matchString() {
+  var string = document.getElementById('matchText').innerHTML;
+  var target = document.getElementById('matchReplace');
+  var replace = string.match(/i/g);
+  target.innerHTML = "Result: " + replace;
+}
+
 function getRepeat() {
   var count = document.getElementById('repeatCount').value;
   var string = document.getElementById('repeatText').innerHTML;
@@ -230,4 +253,156 @@ function searchStringWithRegexp() {
   var target = document.getElementById('searchReplace');
   var replace = string.search(/e|o/g);
   target.innerHTML = "Result: " + replace;
+}
+
+function sliceStrOneParam() {
+  var index = document.getElementById('value10').value;
+  var string = document.getElementById('sliceText').innerHTML;
+  var target = document.getElementById('sliceReplace');
+  var result = string.slice(index);
+
+  target.innerHTML = "Result: " + result;
+}
+
+function sliceStr() {
+  var index1 = document.getElementById('value11').value;
+  var index2 = document.getElementById('value12').value;
+  var string = document.getElementById('sliceText').innerHTML;
+  var target = document.getElementById('sliceReplace');
+  var result = string.slice(index1, index2);
+
+  target.innerHTML = "Result: " + result;
+}
+
+function splitStrOneParam() {
+  var separator = document.getElementById('value13').value;
+  var string = document.getElementById('splitText').innerHTML;
+  var target = document.getElementById('splitReplace');
+  var result = string.split(separator);
+
+  target.innerHTML = "Result: " + result;
+}
+
+function splitStr() {
+  var index1 = document.getElementById('value14').value;
+  var index2 = document.getElementById('value15').value;
+  var string = document.getElementById('splitText').innerHTML;
+  var target = document.getElementById('splitReplace');
+  var result = string.split(index1, index2);
+
+  target.innerHTML = "Result: " + result;
+}
+
+function isStartsWithOneParam() {
+  var searchValue = document.getElementById('startsWithSearchOneParam').value;
+  var string = document.getElementById('startsWithText').innerHTML;
+  var target = document.getElementById('startsWithReplaceOneParam');
+  var result = string.startsWith(searchValue);
+
+  target.innerHTML = "Result: " + result;
+}
+
+function isStartsWith() {
+  var searchValue = document.getElementById('startsWithSearch').value;
+  var lengthValue = document.getElementById('startsWithLength').value;
+  var string = document.getElementById('startsWithText').innerHTML;
+  var target = document.getElementById('startsWithReplace');
+  var result = string.startsWith(searchValue, lengthValue);
+
+  target.innerHTML = "Result: " + result;
+}
+
+function getSubstrOneParam() {
+  var index = document.getElementById('value16').value;
+  var string = document.getElementById('substrText').innerHTML;
+  var target = document.getElementById('substrReplace');
+  var result = string.substr(index);
+
+  target.innerHTML = "Result: " + result;
+}
+
+function getSubstr() {
+  var index1 = document.getElementById('value17').value;
+  var index2 = document.getElementById('value18').value;
+  var string = document.getElementById('substrText').innerHTML;
+  var target = document.getElementById('substrReplace');
+  var result = string.substr(index1, index2);
+
+  target.innerHTML = "Result: " + result;
+}
+
+function getSubstringOneParam() {
+  var index = document.getElementById('value19').value;
+  var string = document.getElementById('substringText').innerHTML;
+  var target = document.getElementById('substringReplace');
+  var result = string.substring(index);
+
+  target.innerHTML = "Result: " + result;
+}
+
+function getSubstring() {
+  var index1 = document.getElementById('value20').value;
+  var index2 = document.getElementById('value21').value;
+  var string = document.getElementById('substringText').innerHTML;
+  var target = document.getElementById('substringReplace');
+  var result = string.substring(index1, index2);
+
+  target.innerHTML = "Result: " + result;
+}
+
+function setToLocaleLowerCase() {
+  var string = document.getElementById('toLocaleLowerCaseText').innerText;
+  var target = document.getElementById('toLocaleLowerCaseReplace');
+  var result = string.toLocaleLowerCase();
+
+  target.innerHTML = "Result: " + result;
+}
+
+function setToLocaleUpperCase() {
+  var string = document.getElementById('toLocaleUpperCaseText').innerText;
+  var target = document.getElementById('toLocaleUpperCaseReplace');
+  var result = string.toLocaleUpperCase();
+
+  target.innerHTML = "Result: " + result;
+}
+
+function setToLowerCase() {
+  var string = document.getElementById('toLowerCaseText').innerText;
+  var target = document.getElementById('toLowerCaseReplace');
+  var result = string.toLowerCase();
+
+  target.innerHTML = "Result: " + result;
+}
+
+function changeToString() {
+  var number = 1234567890;
+  var target = document.getElementById('toStringReplace');
+  var result = number.toString();
+  var typeOfResult = typeof(result);
+
+  target.innerHTML = "Result: " + result + "<br>Type: " + typeOfResult;
+}
+
+function setToUpperCase() {
+  var string = document.getElementById('toUpperCaseText').innerText;
+  var target = document.getElementById('toUpperCaseReplace');
+  var result = string.toUpperCase();
+
+  target.innerHTML = "Result: " + result;
+}
+
+function getTrim() {
+  var string = "   We animals are the most complicated things in the known universe.   ";
+  var target = document.getElementById('trimReplace');
+  var result = string.trim();
+
+  target.innerHTML = "Result: " + result;
+}
+
+function getValueOf() {
+  var string = document.getElementById('valueOfText').innerText;
+  var target = document.getElementById('valueOfReplace');
+  var result = string.valueOf();
+
+  target.innerHTML = "Result: " + result;
 }
