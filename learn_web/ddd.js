dd5c376cb209e1345a40b010b81676576ea6bb10
Sproodigy <sproodigy@gmail.com>
2019-04-27 10:45:06 +0400

    Some refactoring in the bogdansProject

--- learn_web/ddd.js
+++ learn_web/ddd.js
@@ -1,19 +1,37 @@
-var calculator = {
+// var x = 100;
 
-read: function() {
-  this.a = +prompt('a?', 0);
-  this.b = +prompt('b?', 0);
-}
-
-  sum: function() {
-    return this.a + this.b;
-  },
-
-  mul: function() {
-    return this.a * this.b;
+var leftSide = (3.383 * 1000000 + 3.82229 * 1000000 - 1.39832 * 1000000);
+var rightSide = '(3.3878 * 1000000 + 3.8339 * 1000000 - x)';
+// console.log(eval(rightSide));
+for (var x = -1000000; x <= 1000000; x++) {
+//
+//   // console.log(eval(leftSide, ' --- ', rightSide));
+  if (leftSide == +eval(rightSide)) {
+    console.log(leftSide / 1000000 + ' Equal!!! ' + +eval(rightSide) / 1000000);
+    break;
+  }
+  else {
+    console.log(leftSide / 1000000 + ' Not equal! ' + +eval(rightSide) / 1000000, ' -- ' + x);
   }
 }
 
-calculator.read();
-console.log(calculator.sum());
-console.log(calculator.mul());
+
+// var calculator = {
+//
+// read: function() {
+//   this.a = +prompt('a?', 0);
+//   this.b = +prompt('b?', 0);
+// }
+//
+//   sum: function() {
+//     return this.a + this.b;
+//   },
+//
+//   mul: function() {
+//     return this.a * this.b;
+//   }
+// }
+//
+// calculator.read();
+// console.log(calculator.sum());
+// console.log(calculator.mul());
